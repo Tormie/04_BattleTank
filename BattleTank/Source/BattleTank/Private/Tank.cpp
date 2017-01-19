f5839db8b18f6125b942aa5bad97866778f85ba9
Niels Vermeulen <mailforniels@gmail.com>
2017-01-19 11:50:25 +0100

    Lesson 142

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankBarrel.h"
+#include "CannonProjectile.h"
 #include "Tank.h"
 #include "TankAimingComponent.h"
 
@@ -23,6 +25,7 @@ void ATank::BeginPlay()
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret * TurretToSet)
@@ -31,8 +34,16 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 }
 void ATank::Fire()
 {
+	if (!Barrel) { return; }
+
+	// Spawn a projectile at the socket location on the barrel.
 	auto time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("%f: Fire in the hole!"), time);
+	GetWorld()->SpawnActor<ACannonProjectile>(
+		CannonProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+	);
 }
 
 // Called to bind functionality to input
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -10,6 +10,7 @@
 class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
+class ACannonProjectile;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -29,6 +30,9 @@ public:
 	UPROPERTY(EditAnywhere, Category = Firing)
 	float LaunchSpeed = 4000;
 
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<ACannonProjectile> CannonProjectileBlueprint;
+
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
@@ -42,6 +46,7 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	
+	// Local Barrel reference for spawning projectile.
+	UTankBarrel* Barrel = nullptr;
 	
 };
