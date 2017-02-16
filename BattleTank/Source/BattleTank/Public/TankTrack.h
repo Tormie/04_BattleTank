f3152d6403a0d057fbe01852b22119d201f86e54
Niels <mailforniels@gmail.com>
2017-02-16 15:05:53 +0100

    Lesson 176 halfway

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -16,8 +16,17 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	void SetThrottle(float Throttle);
+	void DriveTrack();
+	void UTankTrack::ApplySidewaysForce();
 
 	// Max force per track in Newtons.
 	UPROPERTY(EditDefaultsOnly)
 	float TrackMaxDrivingForce = 400000; // Assume 40 ton tank and 1g accelleration.
+
+private:
+	UTankTrack();
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	float CurrentThrottle = 0;
 };
